310562a173d0327379547f971c33030ca6239613
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-22 15:43:04 +0000

    adds book_store project

---
+++ book_store/spec/seeds.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE books RESTART IDENTITY; 
+
+
+INSERT INTO books (title, author_name) VALUES ('Harry Potter', 'J.K. Rowling');
+INSERT INTO books (title, author_name) VALUES ('The Woman in White', 'Wilkie Collins');
